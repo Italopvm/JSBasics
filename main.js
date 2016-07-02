e9be05968ca781d22595f88592dcca3bedc3ca5d
italopvm <italopvm@gmail.com>
2016-07-02 13:30:27 -0600

    second commit

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 alert( "Hello Boulder!");
+
+function greeting(){
+
+	var name = prompt("give me your name");
+    var lastname= prompt("give me your last name") ;
+     alert(" hello " + name +" "+ lastname);
+
+}
+
+
